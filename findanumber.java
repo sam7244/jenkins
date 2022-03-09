f32789f67e3c5377a6cc3aeadec91a129636f7c2
Shamith kumar Jain K P <73771821+sam7244@users.noreply.github.com>
2022-03-09 11:47:37 +0530

    final2 commit

--- findanumber.java
+++ findanumber.java
@@ -10,7 +10,7 @@ class findanumber {
   public void linearSearch(int[] intArray, int value) {
     for(int i=0; i<intArray.length; i++) {
       if (intArray[i] == value) {
-        System.out.println("Value is found at the index of " + i);
+        System.out.println("Value is foundssss at the index of " + i);
         return;
       }
     }
